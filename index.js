70736fcf6b241c4bb1a8657d11b959c45f1f71cf
Maremar3 <153134313+Maremar3@users.noreply.github.com>
2024-01-21 22:27:09 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,169 +1,67 @@
 let data =
   'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctors Assistant,26';
-  //-------------one---------------------------------//
+  //-------------one AND Two---------------------------------//
   let array=data.split("\n");
   //console.log(array);
-
   let rows=[]
+  
+//Instead of hard-coding four columns per row, expand your code to accept any number of columns.
   for(let i =0;i<array.length;i++){
+    //Store your results in a two-dimensional array.
   rows.push(array[i].split(","))
 }
-
-console.log(rows);
-//Cache this two-dimensional array in a variable for later use.
+console.log(rows)
 oldarray=rows
-//-----------------Two--------------------------//
-let objstr1
-for(let i=1;i<=4;i++){
-    
-    for(let j=0;j<4;j++){
-       objstr1={ID:rows[i][0],Name:rows[i][1],Occupution:rows[i][2],Age:rows[i][3]}
-       
+
+//-----------------three-----------------------//
+//declaring resulting object
+let obj = {};
+let arr = [];
+//goes for all rows in table except the first one
+for (let i = 1; i < rows.length; i++){
+    obj = {};
+    for(let j = 0; j < rows[0].length; j++){
+//create an object that has “key” as j element in 0 row and “value” as j-element in i row
+    obj[rows[0][j].toLowerCase()] = rows[i][j];
     }
-       console.log(objstr1)
-    
-    }
-    
-
-//--------------------------------Three------//
-//For each row of data in the result array ,create an object where the key of each value is the heading for that value’s column.
-//let objstr1
-let idd=[]
-var lowercase
-let enteries
-let key,keys
-let result={}
-for(let i=1;i<=4;i++){
-    
-for(let j=0;j<4;j++){
-   objstr1={ID:rows[i][0],Name:rows[i][1],Occupution:rows[i][2],Age:rows[i][3]}
-  keys = Object.keys(objstr1);
- key = keys[j];
-  //Convert these keys to all lowercase letters for consistency.
-  //Store these objects in an array, in the order that they were originally listed.
-    result[key.toLowerCase()] = objstr1[key];
+//add worker at the end of our resulting array
+    arr.push(obj);
+    //console.log(obj)
 }
+console.log(arr);
 
-console.log(result)
-//console.log(keys)
+//------------------Four-------------------//
+//Remove the last element from the sorted array.
+let remove=arr.pop()
+
+
+let pp={ id: "48", name: "Barry", occupation: "Runner", age: "25" }
+//Insert the following object at index 1:
+arr.splice(1,0,pp)
+//Add the following object to the end of the array:
+let per={id: "7", name: "Bilbo", occupation: "None", age: "111" }
+let add=arr.push(per)
+console.log(arr)
+//calculate the average
+let average;
+let intage=0
+ for (let p=0;p<arr.length;p++){
+    intage+=parseInt(arr[p].age)
+    average=intage/arr.length
+    //console.log(Object.values(workers))
+ }
+ /////console.log(average)
+ //---------------five---------------
+ //transform the final set of data back into CSV format.
+ /////console.log(arr)
+ let s = arr.map(getd);
+function getd(rows) {
+  return [rows.id,rows.name,rows.occupation,rows.age].join(",");
 }
-//-----------------------four--------------------//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//let key, keys = Object.keys(objstr1);
-//let i
-
-//let result={}
-
-
-
-// for( i=1;i<=4;i++){
-// for(let j=0;j<4;j++){
-
-//     key = keys[j];
-  
-//     result[key.toLowerCase()] = objstr1[key];
- 
-// }
-// //console.log(result);  
-// }
-
-
- 
-   // total--;
-//while(total>=0)
-
-
-
-
-
-// for(let i=1;i<=4;i++){
-    
-//     for(let j=0;j<4;j++){
- 
-//         result[key.toLowerCase()] = objstr1[key];   }
-// }
-
-
-
-//Convert these keys to all lowercase letters for consistency.
-// let idd=[]
-let iden = [42, 57, 63,98];
-let namee = ["Bruce","Bob","Blaine","Bill"];
-let occupationn = ["Knight", "Fry Cook", "Quiz Master", "Doctor"];
-let agee = [41,19,58,26];
-
-for(let conv=0;conv<4;conv++)
-{
-////stored lowercase object in array
-    idd[conv]=rows[0][conv].toLowerCase()
-}
-//console.log(idd)
-//Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
-
-
-   // let idd=rows[0][i].toLowerCase()
-   // console.log(idd)
-
-//-----------------------
-let id = [42, 57, 63,98];
-let name = ["Bruce","Bob","Blaine","Bill"];
-let occupation = ["Knight", "Fry Cook", "Quiz Master", "Doctor"];
-let age = [41,19,58,26];
-let obj = Object.assign({id, name, occupation, age});
-let newArray = [obj];
-//console.log(JSON.stringify(obj));
-//console.log(newArray)
-
-//-------------------------------
-
-
-// let id=[42,54,21,68]
-// let Name= ["Bruce","Bob","Blaine","Bill"];
-// let occupation = ["Knight", "Fry Cook", "Quiz Master", "Doctor"];
-// let age = [41,19,58,26];
-// let obj = Object.assign({id, Name, occupation, age});
-// console.log(obj)
-// let newArray = [obj];
-//console.log(JSON.stringify(obj));
-//console.log(newArray)
-//--------------------------------------------
-// let str110=str1[0]
-//     let str111=str1[1]
-//     let str112=str1[2]
-//     let str113=str1[3]
-//     const objstr1={str110:str2[0],str111:str2[1],str112:str2[2],str113:str3[3]}
-
-
-
-//--------------------------------------------
-//for(colnum=0;colnum<4;colnum++){
-    //console.log(originalstr[i])
-    // if (originalstr[colnum]=='/n')
-    // break;
-    // }
-    //     console.log(colnum);
-
-
-// let columns=[];
-// for(let i=0;i<[rows.length];i++){
-//     columns.push(rows[i][0].split(','))
-
-// }
-// //console.log(columns)
-// //let table=columns.flat()
-// //console.log(table)
-// console.log(array);
+//  let show = arr.map( (columns)=>{ [columns.id,columns.name,columns.occupation,columns.age].join(",")} );
+let title = "id,name,occupation,age"
+s.unshift(title)
+// return
+ console.log(s)
+ let csv=s.toString();
+ console.log(csv)
